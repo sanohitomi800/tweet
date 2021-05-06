7afba9fa610bec920e3f2eabbd80324f3aad3e84
sanohitomi800 <h.tommy.0830s@gmail.com>
2021-05-06 17:42:11 +0900

    詳細機能実装完了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  
+  before_action :set_post, only: [:edit, :show]
   def index
     @posts = Post.all
   end
@@ -7,24 +7,34 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+  
   def create
     Post.create(post_params)    
   end
+  
   def destroy
     post = Post.find(params[:id])
     post.destroy 
   end
+  
   def edit
-    @post = Post.find(params[:id]) 
   end
+  
   def update
     post = Post.find(params[:id])
     post.update(post_params)
   end
   
+  def show 
+  end
   
   private
+  
   def post_params
     params.require(:post).permit(:name, :image, :text)
+  end
+  
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
