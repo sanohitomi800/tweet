1d3f1d797e5b93a7b6dcbac6fc00d8a05a907c84
sanohitomi800 <h.tommy.0830s@gmail.com>
2021-05-06 17:11:33 +0900

    編集機能実装完了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,12 @@ class PostsController < ApplicationController
   def edit
     @post = Post.find(params[:id]) 
   end
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+  end
+  
+  
   private
   def post_params
     params.require(:post).permit(:name, :image, :text)
